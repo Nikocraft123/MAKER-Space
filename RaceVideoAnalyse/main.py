928f0f4c842967594c520238e9cb6ed312cc527c
Nikocraft123 <nikolas_beyer@gmx.de>
2021-06-19 12:22:30 +0200

    Updated RaceVideoAnalyse and requirements.txt

--- RaceVideoAnalyse/main.py
+++ RaceVideoAnalyse/main.py
@@ -1,6 +1,5 @@
 # IMPORTS
 import cv2
-import pyqrcode
 from pyzbar import pyzbar
 
 
@@ -27,7 +26,14 @@ def run():
             # Hole Position und Größe des Qr Code
             x_pos, y_pos, width, height = code.rect
 
-            # 
+            # Zeichne Rahmen um Qr Code
+            cv2.rectangle(frame, (x_pos, y_pos), (x_pos + width, y_pos + height), (0, 0, 255), 2)
+
+            # Decode Qr Code Data
+            qr_data = code.data.decode("utf-8")
+
+            # Write the Data above the Qr Code
+            cv2.putText(frame, qr_data, (x_pos, y_pos - 8), cv2.QT_FONT_NORMAL, 0.4, (0, 255, 0), 1)
 
         # Zeige Video in einem Fenster
         cv2.imshow("MAKER Space - Race Video Analyse", frame)
@@ -48,29 +54,8 @@ def run():
 # MAIN
 if __name__ == "__main__":
 
-    # Definiere Video Signale
+    # Definiere Video Signal
     video_cap = cv2.VideoCapture(0)
-    qrcode_img = cv2.imread(".\qrcode.png")
 
     # Run die Webcam
-    # run()
-
-    # TEMP
-
-    # Load Qr Code Image
-    qrcodes = pyzbar.decode(qrcode_img)
-    for qrcode in qrcodes:
-        x, y, width, height = qrcode.rect
-        cv2.rectangle(qrcode_img, (x, y), (x + width, y + height), (0, 255, 0), 4)
-        qrdata = qrcode.data.decode("utf-8")
-        qrtype = qrcode.type
-        cv2.putText(qrcode_img, qrdata, (x, y - 10), cv2.QT_FONT_NORMAL, 0.5, (255, 0, 0), 1)
-    cv2.imshow("Qr Code", qrcode_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # Create Qr Code
-    i = pyqrcode.create("Car_ID_#1")
-    f = open(r".\qrcode.png", "wb")
-    i.png(f, 10)
-    print(i.terminal())
+    run()
