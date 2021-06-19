48598ad4aeca26b2000bc363414e4e03eb0a6bd7
Nikocraft123 <nikolas_beyer@gmx.de>
2021-06-19 18:40:53 +0200

    Update RaceVideoAnalyse with Class

--- RaceVideoAnalyse/main.py
+++ RaceVideoAnalyse/main.py
@@ -1,61 +1,115 @@
 # IMPORTS
-import cv2
 from pyzbar import pyzbar
+import cv2 as cv
+import pygame as pg
 
 
 # CLASSES
 
+# Application
+class Application:
 
-# METHODS
+    # CONSTRUCTOR
+    def __init__(self):
 
-# Run
-def run():
+        # Definiere App Titel
+        self.title = "MAKER Space - Race Video Analyse"
 
-    # Unendliche Schleife
-    while True:
+        # Definiere App Größe
+        self.width = 900
+        self.height = 700
 
-        # Hole aktuellen Frame
-        _, frame = video_cap.read()
+        # Definiere Kamerasignal
+        self.video_cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 
-        # Lade Qr Codes
-        codes = pyzbar.decode(frame)
+        # Initialisiere Pygame
+        pg.init()
+
+        # Definiere Running
+        self.running = True
+
+
+    # METHODS
+
+    # Run
+    def run(self):
+
+        # Definiere Fenster
+        self.screen = pg.display.set_mode((self.width, self.height), pg.RESIZABLE)
+        pg.display.set_caption(self.title)
+
+        # App Routine
+        while self.running:
+
+            # Aktualisiere Frame
+            _, self.frame = self.video_cap.read()
+
+            # Lade Qr Codes
+            self.load_qr_codes()
+
+            # Konvertiere OpenCV Bild zu Pygame Bild
+            self.pg_frame = pg.image.frombuffer(self.frame.tobytes(), self.frame.shape[1::-1], "BGR")
+
+            # Aktualisiere Fenster Größe
+            self.update_window_size()
+
+            # Zeichne Frame auf dem Fenster (In der Mitte)
+            self.screen.blit(self.pg_frame, ((self.screen.get_width() - self.pg_frame.get_width()) // 2, (self.screen.get_height() - self.pg_frame.get_height()) // 2))
+
+            # Aktualisiere Fenster
+            pg.display.flip()
+
+            # Schaue, ob das Fenster geschlossen worden ist
+            if pg.event.get(pg.QUIT):
+
+                # Setze Running auf Falsch
+                self.running = False
+
+        # Löse das Kamerasignal
+        self.video_cap.release()
+
+    # Lade Qr Codes
+    def load_qr_codes(self):
+
+        # Endcode Frame
+        self.qr_codes = pyzbar.decode(self.frame, [pyzbar.ZBarSymbol.QRCODE])
 
         # Gehe durch jeden Qr Code durch
-        for code in codes:
+        for code in self.qr_codes:
 
-            # Hole Position und Größe des Qr Code
+            # Hole Position und Größe des Qr Codes
             x_pos, y_pos, width, height = code.rect
 
-            # Zeichne Rahmen um Qr Code
-            cv2.rectangle(frame, (x_pos, y_pos), (x_pos + width, y_pos + height), (0, 0, 255), 2)
+            # Zeichne roten Rahmen um Qr Code
+            cv.rectangle(self.frame, (x_pos, y_pos), (x_pos + width, y_pos + height), (0, 0, 255), 2)
 
-            # Decode Qr Code Data
-            qr_data = code.data.decode("utf-8")
+            # Endcode den Inhalt des Qr Codes
+            data = code.data.decode("utf-8")
 
-            # Write the Data above the Qr Code
-            cv2.putText(frame, qr_data, (x_pos, y_pos - 8), cv2.QT_FONT_NORMAL, 0.4, (0, 255, 0), 1)
+            # Schreibe den Inhalt in grün über den Qr Code
+            cv.putText(self.frame, data, (x_pos, y_pos - 5), cv.QT_FONT_NORMAL, 0.4, (0, 255, 0), 1)
 
-        # Zeige Video in einem Fenster
-        cv2.imshow("MAKER Space - Race Video Analyse", frame)
+    # Aktualisiere Fenster Größe
+    def update_window_size(self):
 
-        # Schaue ob eine die Taste 'q' gedrückt worden ist
-        if cv2.waitKey(1) & 0xff == ord("q"):
+        # Hole Größe
+        self.width, self.height = pg.display.get_window_size()
 
-            # Breche ab
-            break
+        # Kontrolliere, ob die Größe zu klein ist
+        if self.width < self.pg_frame.get_width():
+            self.width = self.pg_frame.get_width()
+        if self.height < self.pg_frame.get_height():
+            self.height = self.pg_frame.get_height()
 
-    # Löse das Kamerasignal
-    video_cap.release()
-
-    # Zerstöre alle Fenster
-    cv2.destroyAllWindows()
+        # Setze die neue Größe auf das Fenster
+        self.screen = pg.display.set_mode((self.width, self.height), pg.RESIZABLE)
 
 
 # MAIN
 if __name__ == "__main__":
 
-    # Definiere Video Signal
-    video_cap = cv2.VideoCapture(0)
+    # Initialisiere App
+    app = Application()
 
-    # Run die Webcam
-    run()
+    # Starte App
+    app.run()
